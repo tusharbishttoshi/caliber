6c757e2b3f1695602b81da3c9c9850788f79b30c
Tushar Bisht <tusharbisht276@gmail.com>
2023-06-13 19:29:35 +0530

    remove unwanted css from themes

--- wp-content/themes/hurtado/functions.php
+++ wp-content/themes/hurtado/functions.php
@@ -2,7 +2,11 @@
 function hunt_enqueue_styles()
 {
 	wp_enqueue_style('style', get_stylesheet_uri());
+	wp_enqueue_style('index', get_template_directory_uri() . '/assets/css/index.css');
 	wp_enqueue_style('main-asset-css', get_template_directory_uri() . '/assets/css/main.css');
+	wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
+	wp_enqueue_style('animate-css', get_template_directory_uri() . '/assets/css/animate.css');
+	wp_enqueue_style('font-awesome', get_template_directory_uri() . '/assets/css/font-awesome.css');
 }
 add_action('wp_enqueue_scripts', 'hunt_enqueue_styles');
 ?>
